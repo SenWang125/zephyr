96abdb3fa9ea811b79876778a3a4eb9eb1904dec
Sen Wang <senwang125@gmail.com>
2026-09-23 21:26:44 -0500

    soc: ti: k3: move the CTRL_MMR kick values to the header
    Drivers that write a locked CTRL_MMR partition need the same unlock
    values, and the lock value to restore it afterwards.

--- soc/ti/k3/common/peripherals/ctrl_partitions.h
+++ soc/ti/k3/common/peripherals/ctrl_partitions.h
@@ -7,6 +7,10 @@
 #ifndef __K3_CTRL_PARTITIONS_H_
 #define __K3_CTRL_PARTITIONS_H_
 
+#define K3_CTRL_MMR_KICK0_UNLOCK_VAL (0x68EF3490U)
+#define K3_CTRL_MMR_KICK1_UNLOCK_VAL (0xD172BC5AU)
+#define K3_CTRL_MMR_KICK_LOCK_VAL   (0x00000000U)
+
 void k3_unlock_all_ctrl_partitions(void);
 
 #endif /* __K3_CTRL_PARTITIONS_H */
